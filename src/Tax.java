fb18ae2b84f15e2dcb44ccf0390ef89982eec2bb
Cindy Colucy <ckcolucy@gmail.com>
2017-09-19 12:12:40 -0400

    "lesson 4 setup"

---
+++ src/Tax.java
@@ -0,0 +1,24 @@
+
+public class Tax {
+	double grossIncome;
+	String state;
+	int dependents;
+	static int customerCounter;
+
+	Tax(double gi, String st, int depen) {
+		grossIncome = gi;
+		state = st;
+		dependents = depen;
+		customerCounter++;
+		System.out.println("Preparing the tax data for cutomer #" + customerCounter);
+	}
+
+	public double calcTax() {
+		return (grossIncome * 0.33 - dependents * 100);
+	}
+
+	public static void convertToEuros(double taxInDollars) {
+		System.out.println("Tax in Euros: " + taxInDollars / 1.25);
+	}
+
+}
